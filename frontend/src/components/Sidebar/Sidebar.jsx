7e8f4406b95f94f3d936fe846cd5171ef4a2059f
Mevakh <omerhs2001@gmail.com>
2024-01-04 21:14:13 +0300

    Sidebar update

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -5,20 +5,26 @@ import axios from "axios";
 import "./Sidebar.css"; // Slider için özel stiller
 
 const Sidebar = () => {
+  const logo =
+    "https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?w=826&t=st=1704389437~exp=1704390037~hmac=b665dcfa99c1eddeb49d25c1c0f172f85687e4fcab92d53611867d0e1ba64601";
   return (
-    <div>
-      <div className="flex h-screen w-64  border-r-2">
-        <div className="flex flex-col">
-          <div className="mt-5 ml-5 mb-5">
-            <span>Logo</span>
-          </div>
-          <div className="flex flex-col m-5 gap-3 ">
-            <span>Ev</span>
-            <span>Görevler</span>
-            <span>İstatistik</span>
-          </div>
+    <div className="h-screen flex">
+      <div className="w-64 px-8 py-4 border-r">
+        <div>
+          <img src={logo} alt="" className="w-9 h-9" />
         </div>
+        <nav className="mt-8">
+          <h3 className="text-sm">A</h3>
+          <div className="mt-2 -m-3">
+            <a href="#" className="flex justify-between items-center px-3">
+              <span>Home</span>
+              <span>Dashboard</span>
+            </a>
+          </div>
+        </nav>
       </div>
+
+      <div className="flex-1 min-w-0">C</div>
     </div>
   );
 };
